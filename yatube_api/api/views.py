ba94548428f613b0cf57e87b261d20e4aba08f6f
Daniil Tropin <Rockur@ya.ru>
2024-10-02 00:17:23 +0300

    Now the user sees only his followings

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,8 @@
 from django.db.models import QuerySet
+from django.db.models.manager import BaseManager
 from django.shortcuts import get_object_or_404
 from posts.models import Comment, Follow, Group, Post, User
+from rest_framework import status
 from rest_framework.exceptions import ValidationError
 from rest_framework.filters import SearchFilter
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
@@ -63,20 +65,33 @@ class GroupViewSet(ReadOnlyModelViewSet):
 
 
 class FollowViewSet(GenericViewSet, CreateModelMixin, ListModelMixin):
-    queryset = Follow.objects.all()
     serializer_class = FollowSerializer
     permission_classes = (IsAuthenticated,)
     filter_backends = (SearchFilter,)
     search_fields = ('following__username',)
 
+    def get_queryset(self) -> BaseManager[Follow]:
+        return Follow.objects.filter(user=self.request.user)
+
     def perform_create(self, serializer) -> None:
         following_username = self.request.data.get('following')
+
         try:
-            following = User.objects.filter(username=following_username)[0]
+            following = User.objects.get(username=following_username)
         except User.DoesNotExist:
-            raise ValueError('User not found', code=401)
+            raise ValidationError(
+                {'detail': 'User not found'}, code=status.HTTP_400_BAD_REQUEST
+            )
 
         if self.request.user == following:
             raise ValidationError('You cannot follow yourself')
+
+        if Follow.objects.filter(
+            user=self.request.user, following=following
+        ).exists():
+            raise ValidationError(
+                'You are already following this user',
+                code=status.HTTP_400_BAD_REQUEST,
+            )
 
         serializer.save(user=self.request.user, following=following)
