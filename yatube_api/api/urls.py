4efac227b765b5ff3098d4d671bb5161bf311aaa
Daniil Tropin <Rockur@ya.ru>
2024-10-01 22:14:40 +0300

    Refactor URL routing for API endpoints

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,18 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+
+router_V1 = routers.DefaultRouter()
+router_V1.register('posts', PostViewSet, basename='posts')
+router_V1.register(
+    r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments'
+)
+router_V1.register('groups', GroupViewSet, basename='groups')
+router_V1.register('follow', FollowViewSet, basename='followings')
 
 urlpatterns = [
+    path('v1/', include(router_V1.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
